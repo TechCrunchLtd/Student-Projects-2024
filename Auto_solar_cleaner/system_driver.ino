9bf1d2b016f1697c2adfe74e57beb8363273b84e
Emmanuel <haboyaemmanuel@gmail.com>
2024-06-15 13:45:37 +0300

    Final code

--- Auto_solar_cleaner/system_driver.ino
+++ Auto_solar_cleaner/system_driver.ino
@@ -8,11 +8,10 @@
 
 #define START_POSITION_SENSOR   A5
 #define STOP_POSITION_SENSOR   A4
-#define FRONT_MOTOR_PIN_1       2
-#define FRONT_MOTOR_PIN_2       4
-#define BACK_MOTOR_PIN_1       5
-#define BACK_MOTOR_PIN_2       6
-#define DISTANCE_TIME_mS        5000
+#define MOTOR_PIN               6
+#define MOTOR_SPEED             255
+// #define BACK_MOTOR_PIN_1       5
+#define DISTANCE_TIME_mS        10000
 
 bool forward_movement;
 bool backward_movement;
@@ -21,12 +20,8 @@ unsigned long distance_timer;
 
 void stopMovement(void)
 {
-    //stop motor movement
-    digitalWrite(FRONT_MOTOR_PIN_1, HIGH);
-    digitalWrite(FRONT_MOTOR_PIN_2, LOW);
-    digitalWrite(BACK_MOTOR_PIN_1, HIGH);
-    digitalWrite(BACK_MOTOR_PIN_2, LOW);
-
+    digitalWrite(MOTOR_PIN, LOW);
+    
     forward_movement = false;
     backward_movement = false;
 }
@@ -64,16 +59,14 @@ bool reachedStartPoint(void)
     //     return true;
     // }
 
-    return timeElapsed();
+    // return timeElapsed();
+    return true;
 }
 
 bool driver_initialise(void)
 {
     //setup movement motor
-    pinMode(BACK_MOTOR_PIN_1, OUTPUT);
-    pinMode(BACK_MOTOR_PIN_2, OUTPUT);
-    pinMode(FRONT_MOTOR_PIN_1, OUTPUT);
-    pinMode(FRONT_MOTOR_PIN_2, OUTPUT);
+    pinMode(MOTOR_PIN, OUTPUT);
 
     //setup position sensors
 
@@ -87,10 +80,7 @@ bool driver_moveForward(void)
     //move forward
     if(forward_movement == false)
     {
-        digitalWrite(FRONT_MOTOR_PIN_1, LOW);
-        digitalWrite(FRONT_MOTOR_PIN_2, LOW);
-        digitalWrite(BACK_MOTOR_PIN_1, LOW);
-        digitalWrite(BACK_MOTOR_PIN_2, LOW);
+        analogWrite(MOTOR_PIN, MOTOR_SPEED);
 
         forward_movement = true;
         backward_movement = false;
@@ -108,12 +98,10 @@ bool driver_moveForward(void)
 bool driver_moveBackward(void)
 {
     //move backwards
-    if(backward_movement == false)
+   /* if(backward_movement == false)
     {
         digitalWrite(FRONT_MOTOR_PIN_1, HIGH);
         digitalWrite(FRONT_MOTOR_PIN_2, HIGH);
-        digitalWrite(BACK_MOTOR_PIN_1, HIGH);
-        digitalWrite(BACK_MOTOR_PIN_2, HIGH);
 
         backward_movement = true;
         forward_movement = false;
@@ -123,8 +111,8 @@ bool driver_moveBackward(void)
     {
         stopMovement();
         return true;
-    }
-    return false;
+    }*/
+    return true;
 }
 
 /* ----------------------------------- EOF ---------------------------------- */
